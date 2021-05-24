6c962349a9d5ee9735112b537470001c8e3eefe1
Skyler Calaman <54462713+Blckbrry-Pi@users.noreply.github.com>
2021-05-24 09:25:26 -0400

    Implement settings.

--- ts/sketch.ts
+++ ts/sketch.ts
@@ -1,8 +1,11 @@
 import * as p5Global from "p5/global";
 import * as p5 from "p5";
+
 import State from "./states/states.js";
 import splashScreen from "./states/splashScreen.js";
-import ResourceObj from "./resourceLoader.js";
+
+import ResourceObj from "./game_objects/resourceLoader.js";
+import SettingsObj from "./game_objects/settings.js";
 
 let currState: State;
 
@@ -15,6 +18,7 @@ function preload(): void {
 function setup(): void {
   createCanvas(windowWidth, windowHeight);
   currState = new splashScreen(undefined, resources);
+  
 }
 
 function draw(): void {
@@ -34,12 +38,14 @@ function windowResized(): void {
 declare global {
   interface Window {
     p5: any;
+    gameSettings: SettingsObj;
     preload: ()=>void;
     setup: ()=>void;
     draw: ()=>void;
     windowResized: ()=>void;
   }
 }
+window.gameSettings = new SettingsObj();
 window.preload = preload;
 window.setup = setup;
 window.draw = draw;
